b4e095cc264e325e46910d0b4c5bc53e971bc028
DvachedTrip <katanatip7@gmail.com>
2026-03-09 00:28:08 +0600

    finished project

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+    const discount = 1 - (purchase.discount / 100)
+    return purchase.sale_price * purchase.quantity * discount 
 }
 
 /**
@@ -16,6 +17,15 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
+    if (index === 0) {
+        return seller.profit * 0.15;
+    } else if (index === 1 || index === 2) {
+        return seller.profit * 0.10
+    } else if ( index === (total - 1)) {
+        return 0
+    } else {
+        return seller.profit * 0.05
+    }
     // @TODO: Расчет бонуса от позиции в рейтинге
 }
 
@@ -26,19 +36,78 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+    if (
+        !data ||
+        !Array.isArray(data.sellers) ||
+        !Array.isArray(data.products) ||
+        !Array.isArray(data.purchase_records) ||
+        data.sellers.length === 0 ||
+        data.products.length === 0 ||
+        data.purchase_records.length === 0
+    ) {
+        throw new Error('Некорректные входные данные');
+    }
+    if (typeof options !== "object" || options === null) {
+        throw new Error("Опции не были переданы")
+    }
 
-    // @TODO: Проверка наличия опций
+    const { calculateRevenue, calculateBonus } = options; 
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+    if (typeof calculateRevenue !== "function" || typeof calculateBonus !== "function") {
+        throw new Error("Переменные функции не были определенны")
+    }
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellerStats = data.sellers.map(seller => ({
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {}
+    }))
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+    const sellerIndex = Object.fromEntries(sellerStats.map(seller => [seller.id, seller]));
+    const productIndex = Object.fromEntries(data.products.map(product => [product.sku, product]))
 
-    // @TODO: Сортировка продавцов по прибыли
+    data.purchase_records.forEach(record => { 
+        const seller = sellerIndex[record.seller_id]; 
+        seller.sales_count += 1 
+        seller.revenue += record.total_amount
 
-    // @TODO: Назначение премий на основе ранжирования
+        record.items.forEach(item => {
+            const product = productIndex[item.sku]; // Товар
+            const cost = product.purchase_price * item.quantity
+            const revenue = calculateRevenue(item, product)
+            const profit = revenue - cost
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+            seller.profit += profit
+
+            
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0;
+            }
+
+            seller.products_sold[item.sku] += item.quantity 
+            
+        })
+    })
+
+    sellerStats.sort((a,b) => b.profit - a.profit)
+    sellerStats.forEach((seller,index) =>{
+        seller.bonus = calculateBonus(index, sellerStats.length, seller)
+        seller.top_products = Object.entries(seller.products_sold).map(([sku, quantity]) => ({
+            sku,
+            quantity
+        })).sort((a,b) => b.quantity - a.quantity).slice(0,10)
+    })
+
+    return sellerStats.map(seller => ({
+        seller_id: seller.id,
+        name: seller.name,
+        revenue: +seller.revenue.toFixed(2),
+        profit: +seller.profit.toFixed(2),
+        sales_count: seller.sales_count,
+        top_products: seller.top_products,
+        bonus: +seller.bonus.toFixed(2)
+    }))
 }
